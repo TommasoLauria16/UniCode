c4d8cb99aa3300ab210261e53a7f485b81b9840e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 09:20:24 +0100

    codice per Unicode

--- UniCode/Program.cs
+++ UniCode/Program.cs
@@ -9,14 +9,23 @@ namespace UniCode
             //String unicodedString = "This Unicode string has 2 characters outside the" +
             //"ASCII range:\n" +
             //"pi (\u03A0),and Sigma (\u03A3).";
-            Console.WriteLine("inserisci una stringa");
-            string unicode = Console.ReadLine();
+            //Console.WriteLine("inserisci una stringa");
+            //string unicode = Console.ReadLine();
+            //Console.OutputEncoding = System.Text.Encoding.Unicode;
+            //Console.WriteLine(unicode);
+            //Console.WriteLine();
+
             //Console.WriteLine("original string:");
             //Console.OutputEncoding = System.Text.Encoding.Unicode; questa riga di codice è necessaria al fine
             //di far apparire a video il simbolo che è rappresentato dal codice in unicode
-            
+
             //Console.WriteLine(unicodedString);
             //Console.WriteLine();
+            Console.WriteLine("inserisci una stringa");
+            string unicode = Console.ReadLine();
+            
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicode);
 
         }
     }
