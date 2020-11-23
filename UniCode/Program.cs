f2dcc42e8697c4d5369fcc97abef03924a351346
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 08:53:02 +0100

    commenti

--- UniCode/Program.cs
+++ UniCode/Program.cs
@@ -6,14 +6,17 @@ namespace UniCode
     {
         static void Main(string[] args)
         {
-            String unicodedString = "This Unicode string has 2 characters outside the" +
-            "ASCII range:\n" +
-            "pi (\u03A0),and Sigma (\u03A3).";
+            //String unicodedString = "This Unicode string has 2 characters outside the" +
+            //"ASCII range:\n" +
+            //"pi (\u03A0),and Sigma (\u03A3).";
             Console.WriteLine("inserisci una stringa");
-            Console.WriteLine("original string:");
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine(unicodedString);
-            Console.WriteLine();
+            string unicode = Console.ReadLine();
+            //Console.WriteLine("original string:");
+            //Console.OutputEncoding = System.Text.Encoding.Unicode; questa riga di codice è necessaria al fine
+            //di far apparire a video il simbolo che è rappresentato dal codice in unicode
+            
+            //Console.WriteLine(unicodedString);
+            //Console.WriteLine();
 
         }
     }
