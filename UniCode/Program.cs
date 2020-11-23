1603ae9ecba2bb132c0be143018b504998fab07b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 08:45:36 +0100

    inserimento output

--- UniCode/Program.cs
+++ UniCode/Program.cs
@@ -6,7 +6,15 @@ namespace UniCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!") ;
+            String unicodedString = "This Unicode string has 2 characters outside the" +
+            "ASCII range:\n" +
+            "pi (\u03A0),and Sigma (\u03A3).";
+            Console.WriteLine("inserisci una stringa");
+            Console.WriteLine("original string:");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicodedString);
+            Console.WriteLine();
+
         }
     }
 }
